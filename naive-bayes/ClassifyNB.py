47d64f5398d6a41c7529b0e8dcfcff11b367aabc
Sam Pisheh <sam_pisheh@yahoo.com>
2018-09-07 11:36:47 -0700

    Delete ClassifyNB.py

--- naive-bayes/ClassifyNB.py
+++
@@ -1,7 +0,0 @@
-def classify(features_train, labels_train):   
-    ### fit the classifier on the training features and labels
-    ### return the fit classifier
-    from sklearn.naive_bayes import GaussianNB
-    classifier = GaussianNB()
-    classifier.fit(features_train, labels_train)
-    return classifier
